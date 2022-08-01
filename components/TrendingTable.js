0be145b1f0225f253c9d47e14427865a32df54e6
Jesaja Pavlović <jesaja.pavlovic@hva.nl>
2022-08-01 21:38:24 +0200

    create dynamic table

--- components/TrendingTable.js
+++ components/TrendingTable.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+
 import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -7,60 +7,98 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
-import { Container } from '@mui/material';
+import { Container, LinearProgress } from '@mui/material';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { getMarketChart, getTrendingCoins } from '../pages/api/trending';
+import { CurrencyState } from '../contexts/currencyContext';
+import Image from 'next/image';
+import { colorPercentage } from '../utils/colorText';
+import { numberWithCommas } from '../utils/convert';
 
-const columns = [
-    { id: 'name', label: 'Name', minWidth: 170 },
-    { id: 'code', label: 'ISO\u00a0Code', minWidth: 100 },
-    {
-        id: 'population',
-        label: 'Population',
-        minWidth: 170,
-        align: 'right',
-        format: (value) => value.toLocaleString('en-US'),
-    },
-    {
-        id: 'size',
-        label: 'Size\u00a0(km\u00b2)',
-        minWidth: 170,
-        align: 'right',
-        format: (value) => value.toLocaleString('en-US'),
-    },
-    {
-        id: 'density',
-        label: 'Density',
-        minWidth: 170,
-        align: 'right',
-        format: (value) => value.toFixed(2),
-    },
-];
 
-function createData(name, code, population, size) {
-    const density = population / size;
-    return { name, code, population, size, density };
+function createData(coin, idx) {
+
+    return {
+        rank: idx,
+        coin: [coin.name, coin.symbol, coin.image],
+        price: coin.current_price.toFixed(2),
+        '1h': coin.price_change_percentage_1h_in_currency.toFixed(1),
+        '24h': coin.price_change_percentage_24h.toFixed(1),
+        '7d': coin.price_change_percentage_7d_in_currency.toFixed(1),
+        ath: [coin.ath.toFixed(3), coin.ath_change_percentage.toFixed(1)],
+        mktCap: coin.market_cap,
+        '7dGraph': coin.sparkline_in_7d.price
+    };
 }
 
-const rows = [
-    createData('India', 'IN', 1324171354, 3287263),
-    createData('China', 'CN', 1403500365, 9596961),
-    createData('Italy', 'IT', 60483973, 301340),
-    createData('United States', 'US', 327167434, 9833520),
-    createData('Canada', 'CA', 37602103, 9984670),
-    createData('Australia', 'AU', 25475400, 7692024),
-    createData('Germany', 'DE', 83019200, 357578),
-    createData('Ireland', 'IE', 4857000, 70273),
-    createData('Mexico', 'MX', 126577691, 1972550),
-    createData('Japan', 'JP', 126317000, 377973),
-    createData('France', 'FR', 67022000, 640679),
-    createData('United Kingdom', 'GB', 67545757, 242495),
-    createData('Russia', 'RU', 146793744, 17098246),
-    createData('Nigeria', 'NG', 200962417, 923768),
-    createData('Brazil', 'BR', 210147125, 8515767),
-];
+;
 
 export const TrendingTable = () => {
-    const [page, setPage] = React.useState(0);
-    const [rowsPerPage, setRowsPerPage] = React.useState(10);
+    const columns = [
+        { id: 'rank', label: '#', minWidth: 40, align: 'center', format: (val) => ++val },
+        {
+            id: 'coin', label: 'Coin', minWidth: 130, format: ([name, symbol, image]) => {
+                return (<div className='flex flex-wrap'>
+                    <Image
+                        className=''
+                        src={image}
+                        alt={name}
+                        height={20}
+                        width={20}
+                        unoptimized={true}
+                    />
+                    <span className='font-extrabold px-2 text-white'>{name}</span>
+                    <span className='top-1 align-super text-xs'>{symbol?.toUpperCase()}</span>
+
+                </div>)
+            }
+        },
+        { id: 'price', label: 'Price', align: 'right', format: (val) => `${symbol}${numberWithCommas(val)}` },
+        { id: '1h', label: '1h', align: 'center', format: (val) => colorPercentage(val) },
+        { id: '24h', label: '24h', align: 'center', format: (val) => colorPercentage(val) },
+        { id: '7d', label: '7d', align: 'center', format: (val) => colorPercentage(val) },
+        {
+            id: 'ath', label: 'ATH', align: 'right', format: ([ath, percentage]) => {
+                return (
+                    <div className='flex flex-wrap justify-end'>
+                        <span className=''>{symbol}{numberWithCommas(ath)}</span>
+                        <span className='top-1 align-super  pl-1 text-xs'>{colorPercentage(percentage)}</span>
+                    </div>
+                )
+
+            }
+        },
+        { id: 'mktCap', label: 'Market Cap', align: 'right', format: (val) => `${symbol}${numberWithCommas(val)}` },
+        // { id: '7dGraph', label: 'Last 7 days', align: 'center' },
+    ];
+
+
+
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(100);
+    const [loading, setLoading] = useState(true)
+    const [coins, setCoins] = useState([])
+    const [rows, setRows] = useState([])
+
+    const { currency, symbol } = CurrencyState();
+
+
+
+    const fetchCoins = async () => {
+        setLoading(true);
+        const data = await getMarketChart(currency, undefined, true)
+
+        setCoins(data);
+
+        setRows(data.map((x, y) => createData(x, y)))
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        fetchCoins();
+    }, [currency])
+
 
     const handleChangePage = (event, newPage) => {
         setPage(newPage);
@@ -71,55 +109,74 @@ export const TrendingTable = () => {
         setPage(0);
     };
 
+
     return (
         <Container>
             <Paper sx={{ width: '100%', overflow: 'hidden' }}>
-                <TableContainer sx={{ }}>
-                    <Table stickyHeader aria-label="sticky table">
-                        <TableHead>
-                            <TableRow>
-                                {columns.map((column) => (
-                                    <TableCell
-                                        key={column.id}
-                                        align={column.align}
-                                        style={{ minWidth: column.minWidth }}
-                                    >
-                                        {column.label}
-                                    </TableCell>
-                                ))}
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {rows
-                                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                                .map((row) => {
-                                    return (
-                                        <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                                            {columns.map((column) => {
-                                                const value = row[column.id];
+                {loading ?
+
+                    (<LinearProgress />) :
+                    (
+                        <>
+                            <TableContainer sx={{}} className='bg-gray-900'>
+                                <Table stickyHeader aria-label="sticky table" >
+                                    <TableHead >
+                                        <TableRow >
+                                            {columns.map((column) => (
+                                                <TableCell className='bg-blue-600 border-0 font-bold'
+                                                    key={column.id}
+                                                    align={column.align}
+                                                    style={{ minWidth: column.minWidth }}
+                                                >
+                                                    {column.label}
+                                                </TableCell>
+                                            ))}
+                                        </TableRow>
+                                    </TableHead>
+                                    <TableBody>
+                                        {rows
+                                            .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                                            .map((row) => {
                                                 return (
-                                                    <TableCell key={column.id} align={column.align}>
-                                                        {column.format && typeof value === 'number'
-                                                            ? column.format(value)
-                                                            : value}
-                                                    </TableCell>
+                                                    <TableRow
+                                                        hover
+                                                        role="checkbox"
+                                                        tabIndex={-1}
+                                                        key={row.rank}
+                                                    >
+                                                        {columns.map((column) => {
+                                                            const value = row[column.id];
+                                                            return (
+                                                                <TableCell
+                                                                    key={column.id}
+                                                                    align={column.align}
+                                                                    className='text-slate-400'
+                                                                >
+                                                                    {column.format
+                                                                        ? column.format(value)
+                                                                        : value}
+                                                                </TableCell>
+                                                            );
+                                                        })}
+                                                    </TableRow>
                                                 );
                                             })}
-                                        </TableRow>
-                                    );
-                                })}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-                <TablePagination
-                    rowsPerPageOptions={[10, 25, 100]}
-                    component="div"
-                    count={rows.length}
-                    rowsPerPage={rowsPerPage}
-                    page={page}
-                    onPageChange={handleChangePage}
-                    onRowsPerPageChange={handleChangeRowsPerPage}
-                />
+                                    </TableBody>
+                                </Table>
+                            </TableContainer>
+                            <TablePagination
+                                className='bg-blue-500 '
+                                rowsPerPageOptions={[10, 25, 100]}
+                                component="div"
+                                count={rows.length}
+                                rowsPerPage={rowsPerPage}
+                                page={page}
+                                onPageChange={handleChangePage}
+                                onRowsPerPageChange={handleChangeRowsPerPage}
+                            />
+                        </>)}
+
+
             </Paper>
 
         </Container>
