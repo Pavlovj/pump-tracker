9afcedbfe00495a6504495288a0a6e6d09637ca6
Jesaja Pavlović <jesaja.pavlovic@hva.nl>
2022-08-10 17:25:04 +0200

    reworked abbreviate number function

--- utils/convert.js
+++ utils/convert.js
@@ -7,21 +7,23 @@ export const numberWithCommas = (value) => {
     return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
 
-export const abbreviateNumber = (value) => {
-    var newValue = value;
-    if (value >= 1000) {
-        var suffixes = ["", "k", "m", "b", "t"];
-        var suffixNum = Math.floor(("" + value).length / 3);
-        var shortValue = '';
-        for (var precision = 2; precision >= 1; precision--) {
-            shortValue = parseFloat((suffixNum != 0 ? (value / Math.pow(1000, suffixNum)) : value).toPrecision(precision));
-            var dotLessShortValue = (shortValue + '').replace(/[^a-zA-Z 0-9]+/g, '');
-            if (dotLessShortValue.length <= 2) { break; }
-        }
-        if (shortValue % 1 != 0) shortValue = shortValue.toFixed(1);
-        newValue = shortValue + suffixes[suffixNum];
+export const abbreviateNumber = (value, precision = 3, custom_suffixes) => {
+    let newValue = 1200;
+    let suffixes = ["", "K", "M", "B", "T"];
+
+    if (custom_suffixes?.length) {
+        suffixes = Object.assign(suffixes, custom_suffixes)
     }
-    return newValue;
+    let suffixNum = 0;
+    while (newValue >= 1000) {
+        newValue /= 1000;
+        suffixNum++;
+    }
+
+    newValue = newValue.toPrecision(precision);
+
+    newValue += suffixes[suffixNum];
+    return `${newValue}`;
 }
 
 import { sub, getUnixTime } from 'date-fns'
